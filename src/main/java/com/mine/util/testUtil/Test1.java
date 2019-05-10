d61fea8ea5b492349e0f694488cf21857cf3731e
jiangqd <1730339279@qq.com>
2019-05-10 17:59:40 +0800

    增加jwt代码

--- src/main/java/com/mine/util/testUtil/Test1.java
+++ src/main/java/com/mine/util/testUtil/Test1.java
@@ -1,6 +1,12 @@
 package com.mine.util.testUtil;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jiangqd on 2019/3/16.
@@ -10,11 +16,14 @@ public class Test1 {
 
     public static void main(String[] args) throws ClassNotFoundException {
         //查看当前系统类路径中包含的路径条目
-        System.out.println("路径" + System.getProperty("java.class.path"));
+//        System.out.println("路径" + System.getProperty("java.class.path"));
         //调用当前类的类加载器
-        Class typeLoaded = Class.forName("com.mine.util.testUtil.Test1");
+//        Class typeLoaded = Class.forName("com.mine.util.testUtil.Test1");
         //查看被加载的TestBean是被哪个类加载器加载的
-        System.out.println(typeLoaded.getClassLoader());
+//        System.out.println(typeLoaded.getClassLoader());
+//        String repeatTempleteText = "@[受邀者昵称]，你已经参与过该活动，不可重复助力快来生成海报，";
+//        repeatTempleteText =repeatTempleteText.replace("[受邀者昵称]", "悟空");
+//        System.out.println(repeatTempleteText);
     }
 
 }
