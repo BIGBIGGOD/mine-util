371c4d91c2cd113de0d1db39c319aadd4383e355
jiangqd <1730339279@qq.com>
2019-07-02 18:13:36 +0800

    修改jsonlib的工具类

--- src/main/java/com/mine/util/jsonUtil/JsonLibUtil.java
+++ src/main/java/com/mine/util/jsonUtil/JsonLibUtil.java
@@ -1,6 +1,6 @@
 package com.mine.util.jsonUtil;
 
-import com.mine.model.User;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 /**
@@ -24,6 +24,26 @@ public class JsonLibUtil {
 //        }
 //        new User().getName().toString();
         Long.parseLong(null);
-                System.out.println("");
+        System.out.println("");
+    }
+
+    public void  test(String json) {
+        //json转化成JSONObject对象
+        JSONObject jsonObject = JSONObject.fromObject(json);
+        //从JSONObject对象中获取JSONArray数组
+        JSONArray jsonArray = jsonObject.getJSONArray("key1");
+        //从JSONObject对象中根据key值获取String字符串
+        String str1 = jsonObject.getString("key2");
+        //往JSONObject对象中添加键值对数据
+        //put方式的值可以为null
+        jsonObject.put("name", "ceshiname");
+        //element方式的值不可以为null，需要使用空字符串替代
+        jsonObject.element("xxname", "ceshiname");
+        //往JSONArray数组中添加数据
+        jsonArray.add("sss");
+        //将JSONObject转化成String类型
+        String str2 = jsonObject.toString();
+        //判断是否存在对应key值的数据
+        jsonObject.containsKey("key3");
     }
 }
