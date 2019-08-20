63c60467dd09fcc2c14f3b9971bd537c803fe9c5
jiangqingdong <1730339279@qq.com>
2019-08-20 23:31:36 +0800

    学习线程

---
+++ src/main/java/com/mine/util/testUtil/RunableTest.java
@@ -0,0 +1,15 @@
+package com.mine.util.testUtil;
+
+/**
+ * @author jiangqingdong
+ * @version v1.0
+ * @Description
+ * @DATE 2019/8/20 0020 21:31
+ * @Copyright Copyright © 2019 深圳才华有限网络科技股份有限公司. All rights reserved.
+ */
+public class RunableTest implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("我是RunableTest");
+    }
+}
