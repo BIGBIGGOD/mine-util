8de4261ac5ac4bc279fa345c42a120afa922b85b
jiangqingdong <1730339279@qq.com>
2020-09-05 15:21:55 +0800

    提交

---
+++ mine-domain/src/main/java/com/mine/domain/service/FilterChainService.java
@@ -0,0 +1,17 @@
+package com.mine.domain.service;
+
+/**
+ * @author jiangqingdong
+ * @version v1.0
+ * @Description
+ * @DATE 2020/9/3 0003 9:42
+ * @Copyright Copyright © 2019 深圳花儿绽放网络科技股份有限公司. All rights reserved.
+ */
+public interface FilterChainService {
+
+    /**
+     * 过滤链路
+     * @return res
+     */
+    boolean userFilterChain();
+}
---
+++ mine-domain/src/main/java/com/mine/domain/service/impl/FilterChainServiceImpl.java
@@ -0,0 +1,19 @@
+package com.mine.domain.service.impl;
+
+import com.mine.domain.service.FilterChainService;
+
+/**
+ * @author jiangqingdong
+ * @version v1.0
+ * @Description
+ * @DATE 2020/9/3 0003 9:42
+ * @Copyright Copyright © 2019 深圳花儿绽放网络科技股份有限公司. All rights reserved.
+ */
+public class FilterChainServiceImpl implements FilterChainService {
+    @Override
+    public boolean userFilterChain() {
+
+        return false;
+    }
+
+}
